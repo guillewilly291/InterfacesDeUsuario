c5b724c8f90b04491a3201b3128e34982a03e556
Guillermo <guillermodelecea@outlook.es>
2018-12-04 20:36:45 +0100

    Version 6.0

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(function($){
  
  
 
+  
+
   var input = document.getElementById("loginPassword");
 		input.addEventListener("keyup", function(event) {
   			// Cancel the default action, if needed
@@ -25,7 +27,8 @@ $(function($){
   			if(event.keyCode === 13){
   				checkNamePassword();
   			}
-  		});
+      });
+    
   		password.addEventListener("keyup", function(event){
   			event.preventDefault();
   			if(event.keyCode === 13){
@@ -167,193 +170,10 @@ function readURL(input) {
   }
 }
 
-function comprobar(){//Verifica los datos del registro
-  //Guardamos en un array los elementos que son obligatorios
-  var obligatorio= document.getElementsByClassName('obligatorio');
-  var obligatorio2= document.getElementsByClassName('obligatorio2');
-  var bool=false;
-  var general=false;
-  var color=false;
-  if(obligatorio.length != 0){
-  for(var i=0;i<obligatorio.length;i++){//Recorremos cada dato obligatorio del formulario
-  	bool=false;
-    if(obligatorio[i].value == ""){//Un campo obligatorio es vacio
-      bool=true;
-      general=true;
-    }else if(i == 3){//Se verifica la estructura del email
-    	var str = obligatorio[i].value;
-    	var arStr = str.split("@");//Separamos por @
-    	bool = true;
-      general=true;
-
-    	if(arStr.length == 2){//Tiene solo un @
-    		var arStr2 = arStr[1].split(".");//Separamos por .
-    		if(arStr2.length == 2 && arStr2[1] != ""){//Tiene solo un .
-    			bool = false;
-          general=false;
-    		}
-    	}
-    }else if(i == 6){//Campo check
-      if(!obligatorio[i].checked){//El check de terminos y condiciones debe estar activo
-        color=true;
-        general=true;
-      }
-    }else if(i==4){//Campo fecha
-
-      var fecha= obligatorio[i].value.split("-");
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-      var str=""+yyyy.toString()+mm.toString()+dd.toString();
-      var str2=""+fecha[0].toString()+fecha[1].toString()+fecha[2].toString();
-      if(parseInt(str2)>parseInt(str)){//Validamos que la fecha introducida no sea mayor a la actual
-        bool=true;
-        general=true;
-      }
-    }else if(i==1){//Campo contraseña
-
-      var contra=obligatorio[i].value;
-      contra=contra.toUpperCase();//Pasamos todo a mayúsculas
-      if(contra.length<=8){//Tamaño máximo 8
-        for(var j=0;j<contra.length;j++){
-          var value= contra.charAt(j);
-          if((value>='A'&&value<='Z')||(value>='0'&&value<='9')){//Verificamos que solo contiene letras y números
-            continue;
-          }else{
-            bool=true;
-            general=true;
-            break;
-          }
-        }
-      }else{
-        bool=true;
-        general=true;
-      }
-
-    }
-    //Si no se cumplen todos los campos obligatorios se alertará en rojo la casilla incorrecta
-    if(bool){
-    	obligatorio[i].style.backgroundColor= "#f78585";
-    }
-    if(color){
-      obligatorio[i].parentElement.style.backgroundColor= "#f78585";
-    }
-
-
-}
-    if(!general){//Sin errores
-      añadirCuenta(obligatorio);//Añadimos la cuenta
-      return true;
-    }else{
-      return false;
-    }
-}else{
-    //Editar datos
-    for(var i=0;i<obligatorio2.length;i++){
-      bool=false;
-      if(obligatorio2[i].value == ""){
-        bool=true;
-        general=true;
-      }else if (i==1) {//Campo fecha
-        var fecha= obligatorio2[i].value.split("-");
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-        var yyyy = today.getFullYear();
-        var str=""+yyyy.toString()+mm.toString()+dd.toString();
-        var str2=""+fecha[0].toString()+fecha[1].toString()+fecha[2].toString();
-        if(parseInt(str2)>parseInt(str)){//Verificar fecha
-          bool=true;
-          general=true;
-        }
-      }
-      if(bool){
-      	obligatorio2[i].style.backgroundColor= "#f78585";//Marcar casillas incorrectas
-      }
-    }
-    if(!general){//Editar cuenta
-      editarCuenta(obligatorio2);
-      return true;
-    }else return false;
-}
-}
-
-function resetRegistro(){//Borra los valores de un popUp de registro
-
-  var obligatorio= document.getElementsByClassName('obligatorio');
-  var obligatorio2=document.getElementsByClassName('obligatorio2');
-if(obligatorio.length != 0){
-  for(var i=0; i<obligatorio.length;i++){//Recorremos cada campo del formulario
-
-
-    if(i==6){//Campo check
-      obligatorio[i].checked=false;//Desmarcamos el check
-    }else{
-      obligatorio[i].value="";//Valor vacio
-    }
-  }
-
-
-
-}else{
-  $(".inputDataRegistro").css({"background-color": "white"}).fadeIn(2000);
-  for(var i=0; i<obligatorio2.length;i++){
-    obligatorio2[i].value="";
-  }
-}
-var imageInput= document.getElementById('imgInp');
-imageInput.value="";
-$('#blah').attr('src', "images/sinFoto.png");
-}
-
-
-function añadirCuenta(obligatorio){//Función para aladir cuenta (localStorage)
-  debugger;
-  var str="";
-  var imageInput= $("#file-preview")[0].src;//Obtenemos la imagen del usuario
-  for(var i=0; i<obligatorio.length-1;i++){//Añadimos los valores del formulario
-    str+=obligatorio[i].value+";";//Separamos cada campo con ;
-  }
-  str+=imageInput;//Añadimos al final la imagen
-  if(localStorage.getItem(obligatorio[3].value)==null){//Comprobamos si existe el usuario
-    localStorage.setItem(obligatorio[3].value,str); //identificamos a un usuario por su correo electrónico
-    alert("Usuario Guardado");
-  }else{
-    alert("Usuario Existente, utilice otro correo electrónico");//El usuario ya existe
-  }
-
-
-}
-
-function checkUser(){//Comprueba si existe una cookie de un usuario al iniciar sesión
-  var input= document.getElementsByClassName('inputDataSesion');//Cuenta de inicio de sesión
-  var storage=localStorage.getItem(input[0].value);
-  var storage2=localStorage.getItem(input[2].value);
-  //Obtenemos los datos almacenados del usuario
-  if(storage != null){
-  var data=storage.split(";");
-  }
-  if(storage2 != null){
-  var data=storage2.split(";");
-  }
-  if((storage != null && data[1] == input[1].value) || (storage2 != null && data[1] == input[3].value)){//El usuario coincide
-    window.clearInterval(timer);
-    document.location.href="ejercicio1.html";
-    if(storage != null){
-    localStorage.setItem("loggedIn",storage);
-    }
-    if(storage2 != null){
-    localStorage.setItem("loggedIn",storage2);
-    }
-  }else{
-    alert("El usuario o la contraseña no son correctos");//No existe el usuario introducido
-  }
-}
 
 function changePage(){//Función para recuperar los datos del usuario que ha iniciado sesión
     var primero= "AAAA";
-    debugger
+    
     conjunto = localStorage.getItem(localStorage.getItem(primero)).split(";");
     var formulario1 = document.getElementsByClassName('datoFormulario1');
     var formulario2 = document.getElementsByClassName('datoFormulario2');
@@ -363,7 +183,7 @@ function changePage(){//Función para recuperar los datos del usuario que ha ini
     formulario1[0].value = conjunto[3];
     formulario2[2].value = conjunto[4];
     
-    $("#nombreEnCabecera").text("Hola " + localStorage.getItem(localStorage.getItem(primero)).split(";")[5]);
+    $("#nombreEnCabecera").text("Hola, " + localStorage.getItem(localStorage.getItem(primero)).split(";")[5]);
 
     $("#nombreUser").text(conjunto[1]);
   
@@ -417,7 +237,7 @@ function goBackLogged(){
 
 function userLogged(){
   var bool = localStorage.getItem("ZZZZ");
-  debugger
+  
   if(bool=="true"){
     cambiarHeader();
   }else{
@@ -613,7 +433,7 @@ function checkCookieMail(name,email){
 
 function checkCookie(){
   //Returnea un array de strings, que tendran de 0-5, las propiedas de las cookies
-  debugger
+  
   var user = document.getElementById("loginUser").value;
   var password = document.getElementById("loginPassword").value;
   var primero = "AAAA";
@@ -649,10 +469,10 @@ function checkCookie(){
 
 function cambiarHeader() {
   var primero= "AAAA";
-    debugger
+    
     conjunto = localStorage.getItem(localStorage.getItem(primero)).split(";");
    
-    $("#nombreEnCabecera").text("Hola " + localStorage.getItem(localStorage.getItem(primero)).split(";")[5]);
+    $("#nombreEnCabecera").text("Hola, " + localStorage.getItem(localStorage.getItem(primero)).split(";")[5]);
   
     if (conjunto[7] != "" && conjunto[7] != null) {
      
@@ -692,7 +512,7 @@ function checkPassword(password){
 
 
 function createCookie(){
-  debugger;
+  
   var userName= document.getElementById("userName").value;
   var password = document.getElementById("password").value;
   var	nameSurname = document.getElementById("nameSurname").value;
@@ -751,7 +571,7 @@ function seleccionarHotel(idHotel){
 }
 
 function cargaDatos(){
-	
+  window.clearInterval(timer);
 	userLogged();
 	var idHotel = localStorage.getItem("seleccionHotel");
 
@@ -810,7 +630,7 @@ function precioReserva(precioDia){
 }
 
 function insertDates(){
-	debugger;
+	
 
 	var fechaIni = document.getElementById("fecha1").value;
 	var fechaFin = document.getElementById("fecha2").value;
@@ -827,7 +647,7 @@ function insertDates(){
 
  function realizarBusqueda(){
 	userLogged();
- 	debugger;
+  window.clearInterval(timer);
 
  	if(document.getElementById('inputBuscar').value == ''){
 
@@ -912,7 +732,7 @@ function insertDates(){
 
  function aplicarFiltros(){
 
- 	debugger;
+ 	
 
  	filtroEstrellas();
  	insertDates();
@@ -922,7 +742,7 @@ function insertDates(){
 
 
  function resetEstrellas(){
- 	debugger;
+ 	
  	localStorage.setItem('estrellas', 0);
 
  	var arEstrellas = document.getElementsByName('rate');
@@ -934,7 +754,7 @@ function insertDates(){
 
 
  function busquedaHotel(){
- 	debugger;
+ 	
 
  	resetEstrellas();
 
@@ -960,7 +780,7 @@ function insertDates(){
 
 
 function checkRadioButton(e) {
-  debugger;
+  
 
   var arButtons = document.getElementsByClassName('radioButton');
 
@@ -982,7 +802,7 @@ function checkRadioButton(e) {
 }
 
 function editCookie(){
-  debugger;
+  
   
   var formulario1=document.getElementsByClassName("datoFormulario1");
   var formulario2=document.getElementsByClassName("datoFormulario2");
@@ -1029,5 +849,8 @@ function editCookie(){
     document.getElementById("textArea").value = "";
    
   }
-
+function payAndLoad(){
+  window.alert("Se ha realizado el pago correctamente. Gracias por confiar en El Rincón de la Opinión.");
+  document.location.href="paginaPrincipalTest.html";
+}
 
